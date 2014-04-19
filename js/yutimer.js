cb77416a72f978b341733f4fe38a6292ed381360
arose13 <anthony_rose12@hotmail.com>
2014-04-19 15:42:40 -0400

    main.php rename and prettyTime

--- js/yutimer.js
+++ js/yutimer.js
@@ -2,6 +2,16 @@ function getTime(currentDate) {
 	var hours = currentDate.getHours();
 	var minutes = currentDate.getMinutes();
 	var seconds = currentDate.getSeconds();
-	var composedString = hours + ":" + minutes + ":" + seconds;
+	var composedString = hours + ":" + prettySixty(minutes) + ":" + prettySixty(seconds);
 	return composedString;
+}
+
+function prettySixty(t) {
+	var time
+	if(t < 10){
+		time = "0" + t;
+	} else {
+		time = t;
+	}
+	return time;
 }
