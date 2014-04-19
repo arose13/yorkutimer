57c5ebdd50ddbd2673617b4d0994cae5dbb96573
arose13 <anthony_rose12@hotmail.com>
2014-04-19 16:13:56 -0400

    prettyHour()

--- js/yutimer.js
+++ js/yutimer.js
@@ -2,12 +2,22 @@ function getTime(currentDate) {
 	var hours = currentDate.getHours();
 	var minutes = currentDate.getMinutes();
 	var seconds = currentDate.getSeconds();
-	var composedString = hours + ":" + prettySixty(minutes) + ":" + prettySixty(seconds);
+	var composedString = prettyHour(hours) + ":" + prettySixty(minutes) + ":" + prettySixty(seconds);
 	return composedString;
 }
 
+function prettyHour(t) {
+	var time;
+	if(t > 12){
+		time = t - 12;
+	} else {
+		time = t;
+	}
+	return time;
+}
+
 function prettySixty(t) {
-	var time
+	var time;
 	if(t < 10){
 		time = "0" + t;
 	} else {
